b27cc77f3653dd8e545cc2d96b2d60059211cdb3
it24104369 <it24104369@my.sliit.lk>
2024-11-21 15:15:48 +0530

    Add files via upload

--- Q1/IT24104369Lab5Q1.java
+++ Q1/IT24104369Lab5Q1.java
@@ -14,7 +14,7 @@ public class IT24104369Lab5Q1 {
 
         System.out.println("user enterd nombers are: " + num1 +" "+num2 + " "+num3);
         int Smallest= Math.min(num1 , Math.min(num2,num3));
-        int Largest= Math.max(num1, Math.max(num1,num2));
+        int Largest= Math.max(num1, Math.max(num2,num2));
 
         System.out.println("The Small nomber is: " + Smallest);
         System.out.println("The Largest nomber is: " + Largest);
